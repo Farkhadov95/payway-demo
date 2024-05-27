bf5996f197e1f7f08a8cd13f4586d0ddbbff7cab
Farkhadov95 <farkhadov950405@gmail.com>
2024-05-27 20:16:23 +0500

    updated import for dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
